dd14db7841304483b13acd11928ac06510abdd66
Fabiano <fabiano.ssantos79@gmail.com>
2023-05-09 18:52:46 -0300

    init system

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Login from 'pages/Login'
+import Feira from 'pages/Feira'
+import Carrinho from 'pages/Carrinho'
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Login />
+                </Route>
+                <Route path="/feira">
+                    <Feira />
+                </Route>
+                <Route path="/carrinho">
+                    <Carrinho />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
